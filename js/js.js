7b4030760da8a7da9be11e14ab6529bf08e69d2e
unaivan <unaivan22@gmail.com>
2020-11-25 00:19:05 +0700

    upload

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function showSeach(){
+    $('#seacrh').show('slow');
+    $('#showSearch').hide();
+    $('#closeSearch').show('slow');
+}
+
+function hideSeach(){
+    $('#seacrh').hide('slow');
+    $('#showSearch').show('slow');
+    $('#closeSearch').hide();
+}
